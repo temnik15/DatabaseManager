50670479f828d3ac0fa26369fbc204df62e2b799
temnik <temnik15@bk.ru>
2020-06-04 11:46:01 +0300

    class extends Application class from JavaFX

---
+++ src/main/java/ru/temnik/databaseManager/ControllerGUI.java
@@ -0,0 +1,41 @@
+package ru.temnik.databaseManager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
+import ru.temnik.databaseManager.DAO.TableDaoJdbcImpl;
+import ru.temnik.databaseManager.Scenes.AbstractScene;
+import ru.temnik.databaseManager.Scenes.AuthScene;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class ControllerGUI extends Application {
+    private App app;
+    private Stage mainStage;
+    @Setter
+    private AbstractScene currentScene;
+    private Map<String, AbstractScene> allScenes;
+    private  TableDaoJdbcImpl tableDAO;
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        app = new App(this);
+        tableDAO = new TableDaoJdbcImpl(app.getDataSource());
+        mainStage = stage;
+        allScenes = new HashMap<>();
+
+        AuthScene authScene = new AuthScene(this);
+        authScene.init();
+        authScene.show(null);
+        allScenes.put("authScene", authScene);
+        mainStage.show();
+    }
+}
