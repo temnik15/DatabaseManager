bbe402c635c8028033160993ef1a4fa4691a9a05
temnik <temnik15@bk.ru>
2020-06-04 11:44:27 +0300

    App entry point

---
+++ src/main/java/ru/temnik/databaseManager/Runner.java
@@ -0,0 +1,10 @@
+package ru.temnik.databaseManager;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Runner {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        ControllerGUI.main(args);
+
+    }
+}
