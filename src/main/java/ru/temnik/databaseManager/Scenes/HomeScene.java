f6fc5c3dd243bd6f3d73af9a89d73d117b08bea2
temnik <temnik15@bk.ru>
2020-06-12 22:47:00 +0300

    correct TableView for findByParameters and remove more DAOJdbcImpl

--- src/main/java/ru/temnik/databaseManager/Scenes/HomeScene.java
+++ src/main/java/ru/temnik/databaseManager/Scenes/HomeScene.java
@@ -1,18 +1,15 @@
 package ru.temnik.databaseManager.Scenes;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -22,7 +19,6 @@ import ru.temnik.databaseManager.DAO.*;
 import ru.temnik.databaseManager.Entities.*;
 
 import javax.sql.DataSource;
-import javax.xml.soap.Text;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -32,7 +28,6 @@ import java.util.Map;
 public class HomeScene extends AbstractScene {
     private final TableDAO tableDAO;
     private final DataSource dataSource;
-    private final UserDAO userDAO;
     private CrudDAO crudDAO;
     private Map<String, Button> controlBtns;
 
@@ -41,32 +36,38 @@ public class HomeScene extends AbstractScene {
         tableDAO = controllerGUI.getTableDAO();
         controlBtns = new HashMap<>();
         dataSource = controllerGUI.getApp().getDataSource();
-        userDAO = new UserDaoJdbcImpl(dataSource);
-
     }
 
     @Override
     public void init() {
         stage = controllerGUI.getMainStage();
+        //Инициализация сцены
         initNodes();
     }
 
     private void initNodes() {
+        //Создание и настройка основных контейнеров сцены
+        //root - корневой
         BorderPane root = new BorderPane();
+        //topBox - для кнопок CRUD
         HBox topBox = new HBox();
-        VBox leftBox = new VBox();
-        VBox centerBox = new VBox();
-
         topBox.setSpacing(10.0);
+        //Для списка доступных таблиц БД
+        VBox leftBox = new VBox();
+        //Для отображение TableView, а также в него вкладывается topBox
+        VBox centerBox = new VBox();
         centerBox.setSpacing(10.0);
 
 
+
+        //Метка для поиска таблиц
         Label searchTableLabel = new Label("Поиск по таблицам: ");
         nodes.put("searchTableLabel", searchTableLabel);
-
+        //Поле для поиска таблиц
         TextField searchTableField = new TextField();
         nodes.put("searchTableField", searchTableField);
 
+        //CRUD кнопки. Сохраняются в Map, чтоб иметь доступ потом из подметодов.
         Button btnAdd = new Button("Добавить");
         controlBtns.put("btnAdd", btnAdd);
         Button btnChange = new Button("Изменить");
@@ -75,6 +76,7 @@ public class HomeScene extends AbstractScene {
         controlBtns.put("btnDelete", btnDelete);
         Button btnSearch = new Button("Поиск по таблице");
         controlBtns.put("btnSearch", btnSearch);
+        //До выбора таблицы из списка слева все кнопки должны быть не активны
         btnAdd.setDisable(true);
         btnChange.setDisable(true);
         btnDelete.setDisable(true);
@@ -101,7 +103,7 @@ public class HomeScene extends AbstractScene {
             Object objectDao = null;
             switch (entityType) {
                 case DOCUMENT_STATUSES:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, DocumentStatus.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, DocumentStatus.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
@@ -109,47 +111,48 @@ public class HomeScene extends AbstractScene {
                    /* crudDAO=new CrudDAOJdbcImpl(dataSource, User.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;*/
-                    crudDAO=new UserDaoJdbcImpl(dataSource);
+                    crudDAO = new UserDaoJdbcImpl(dataSource);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
                 case DEGREES_OF_SECRECY:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, DegreeOfSecrecy.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, DegreeOfSecrecy.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     objectDao = crudDAO;
                     break;
                 case ACTS_ADMISSION:
-                   // objects = FXCollections.observableArrayList(actsAdmDAO.findAll().toArray());
-                   // objectDao = actsAdmDAO;
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, ActAdm.class);
+                    objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
+                    objectDao = crudDAO;
                     break;
                 case ACTS_EXTRADITION:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, DocumentStatus.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, ActExtr.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
                 case READERS:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, Reader.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, Reader.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
                 case DOCUMENTS:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, Document.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, Document.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
                 case EMPLOYEES:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, Employee.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, Employee.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
                 case PASSPORTS:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, Passport.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, Passport.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
                 case POSITIONS_EMPLOYEES:
-                    crudDAO=new CrudDAOJdbcImpl(dataSource, PositionEmployee.class);
+                    crudDAO = new CrudDAOJdbcImpl(dataSource, PositionEmployee.class);
                     objects = FXCollections.observableArrayList(crudDAO.findAll().toArray());
                     objectDao = crudDAO;
                     break;
@@ -188,8 +191,8 @@ public class HomeScene extends AbstractScene {
 
     private TableView<Object> getTable(ObservableList<Object> objects, Object objectDao) {
         TableView<Object> table = new TableView<>(objects);
-        CrudDAO crudDAO = (CrudDAO)objectDao;
-        Class entityClass =crudDAO.getEntityClass();
+        CrudDAO crudDAO = (CrudDAO) objectDao;
+        Class entityClass = crudDAO.getEntityClass();
         Field[] fields = entityClass.getDeclaredFields();
         configControlBtns(table, fields, objectDao, entityClass);
         for (int i = 0; i < fields.length; i++) {
@@ -243,7 +246,7 @@ public class HomeScene extends AbstractScene {
                         field.set(entity, value);
                     } catch (NumberFormatException e) {
                         try {
-                            field.set(entity, 0);
+                            field.set(entity, -1);
                         } catch (IllegalAccessException ex) {
                             ex.printStackTrace();
                         }
@@ -283,10 +286,10 @@ public class HomeScene extends AbstractScene {
                             TextField textField = new TextField();
                             try {
                                 Type type = fields[i].getType();
-                                if(type.getTypeName().equals("int")){
+                                if (type.getTypeName().equals("int")) {
                                     textField.setText(String.valueOf(fields[i].get(newValue)));
-                                }else{
-                                    textField.setText((String)fields[i].get(newValue));
+                                } else {
+                                    textField.setText((String) fields[i].get(newValue));
                                 }
                             } catch (IllegalAccessException e) {
                                 e.printStackTrace();
@@ -309,7 +312,7 @@ public class HomeScene extends AbstractScene {
                             ObservableList<Node> rowsList = rows.getChildren();
                             for (int i = 1; i < fields.length; i++) {
                                 Field field = fields[i];
-                                VBox row = (VBox) rowsList.get(i-1);
+                                VBox row = (VBox) rowsList.get(i - 1);
                                 TextField textField = (TextField) row.getChildren().get(1);
                                 try {
                                     field.setAccessible(true);
@@ -336,7 +339,7 @@ public class HomeScene extends AbstractScene {
                             CrudDAO crudDAO = (CrudDAO) objDAO;
                             try {
                                 fields[0].setAccessible(true);
-                                fields[0].set(entity,fields[0].get(newValue));
+                                fields[0].set(entity, fields[0].get(newValue));
                             } catch (IllegalAccessException e) {
                                 e.printStackTrace();
                             }
@@ -345,15 +348,14 @@ public class HomeScene extends AbstractScene {
                             objects.addAll(crudDAO.findAll());
 
 
-
                         });
                         rows.getChildren().add(btn);
                         Scene scene = new Scene(rows);
                         stageSearch.setScene(scene);
                         stageSearch.show();
                     });
-                    btnDelete.setOnAction((aeD)->{
-                        CrudDAO crudDAO = (CrudDAO)objDAO;
+                    btnDelete.setOnAction((aeD) -> {
+                        CrudDAO crudDAO = (CrudDAO) objDAO;
                         crudDAO.delete(newValue);
                         objects.clear();
                         objects.addAll(crudDAO.findAll());
@@ -363,7 +365,7 @@ public class HomeScene extends AbstractScene {
             }
         });
         Button btnAdd = controlBtns.get("btnAdd");
-        btnAdd.setOnAction((ae)->{
+        btnAdd.setOnAction((ae) -> {
             Stage stageSearch = new Stage();
             VBox rows = new VBox();
             rows.setSpacing(10.0);
@@ -388,7 +390,7 @@ public class HomeScene extends AbstractScene {
                 ObservableList<Node> rowsList = rows.getChildren();
                 for (int i = 1; i < fields.length; i++) {
                     Field field = fields[i];
-                    VBox row = (VBox) rowsList.get(i-1);
+                    VBox row = (VBox) rowsList.get(i - 1);
                     TextField textField = (TextField) row.getChildren().get(1);
                     try {
                         field.setAccessible(true);
